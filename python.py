c67a8074da6c5fccfa4d75bd78c6d3fb93f8d028
skiff4444 <skiff4444@ukr.net>
2022-08-27 21:11:09 +0300

    Update python.py

--- python.py
+++ python.py
@@ -1,2 +1,13 @@
+from cgi import print_arguments
+
+
 print("Hello world")
 print("It is Oleg")
+print(3+6)
+print(3+6+12)
+print("Oleg" +" " + "has a " + "35 years")
+name_user = input("Write your name:  ")
+age_user = input("Write your age:  ")
+city_user = input("Write your city: ")
+print("Welcom " + name_user + " your age is " + age_user + " you from " + city_user)
+input()
